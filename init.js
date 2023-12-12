5743dc62b401c88dae4685aa94e15a3a8234c86d
chat43CAAG <153575347+chat43CAAG@users.noreply.github.com>
2023-12-11 21:05:59 -0600

    Create init.js

---
+++ init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyDel6sGPRl1yPpIDetfcr6-cIRKdyYZ108",
+  authDomain: "chat43caag.firebaseapp.com",
+  projectId: "chat43caag",
+  storageBucket: "chat43caag.appspot.com",
+  messagingSenderId: "769272920387",
+  appId: "1:769272920387:web:d1f2479524a9ecd98717f6"
+    });
